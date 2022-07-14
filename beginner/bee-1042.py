2afc378b30cf45072ae9bc43544b786fe22725df
Eric Lemos <eric.lemoss@gmail.com>
2022-07-14 12:02:25 -0300

    in progress

---
+++ beginner/bee-1042.py
@@ -0,0 +1,6 @@
+# https://www.beecrowd.com.br/judge/pt/problems/view/1042
+
+#input_numbers = tuple(input().strip().split())
+#a = int(input_numbers[0])
+#b = int(input_numbers[1])
+#c = int(input_numbers[2])
